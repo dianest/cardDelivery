65eab84108a5fcbd07d9d88f827930fe7dd6b263
dianest <nddgdd@gmail.com>
2020-11-23 17:10:40 +0100

    Change test condition

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -13,41 +13,27 @@ import org.openqa.selenium.Keys;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class CardDeliveryTest {
-    @BeforeAll
-    static void setup() {
-        Configuration.timeout = 15000;
-    }
-
-    @AfterAll
-    static void tearDown() {
-        Configuration.timeout = 4000;
-    }
 
     @Test
     public void happyPath() {
         open("http://localhost:9000/");
         SelenideElement form = $(By.tagName("FORM"));
         form.$("[data-test-id=city] input").setValue("Санкт-Петербург");
-        for(int i = 0; i < 10; i++) {
-            form.$("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
-        }
-
+        form.$("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
         String date = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         form.$("[data-test-id=date] input").setValue(date);
-        form.$("[data-test-id=name] input").setValue("Иванов Иван Иванович");
+        form.$("[data-test-id=name] input").setValue("Иванов Иван");
         form.$("[data-test-id=phone] input").setValue("+79876543210");
         form.$("[data-test-id=agreement]").click();
         form.$("[class=button__content]").click();
 
         SelenideElement notification = $("[data-test-id=notification]");
-        notification.shouldBe(
-                Condition.visible,
-                Condition.text("Встреча успешно забронирована на " + date
-                )
-        );
+        notification.waitUntil(Condition.visible, 15000).
+               shouldHave(text("Встреча успешно забронирована на " + date));
     }
 }
